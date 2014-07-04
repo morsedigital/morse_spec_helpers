a4f5f4d7d2c712432e695ded8aa44aec6cab3241
Terry S <itsterry@gmail.com>
2014-07-04 14:27:07 +0100

    Trying to fix nil join problem

--- lib/generators/morse_spec_helpers_generator.rb
+++ lib/generators/morse_spec_helpers_generator.rb
@@ -5,7 +5,7 @@ class MorseSpecHelpersGenerator < Rails::Generators::Base
     image_dir="#{support_dir}/spec/support/images"
     Dir.mkdir(support_dir) unless Dir.exist?(support_dir)
     Dir.mkdir(image_dir) unless Dir.exist?(image_dir)
-    copy_file '../support/project_helpers.rb'."#{support_dir}/project/helpers.rb"
-    copy_file '../support/images/placeholder.png'."#{image_dir}/placeholder.png"
+    copy_file '../support/project_helpers.rb',"#{support_dir}/project/helpers.rb"
+    copy_file '../support/images/placeholder.png',"#{image_dir}/placeholder.png"
   end
 end
