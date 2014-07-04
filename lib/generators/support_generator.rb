47c4dba089ba9a86a4df604784ee4f6abcd085b5
Terry S <itsterry@gmail.com>
2014-07-04 13:19:55 +0100

    Added supprt files

---
+++ lib/generators/support_generator.rb
@@ -0,0 +1,11 @@
+class SupportGenerator < Rails::Generators::Base
+  source_root(File.expand_path(File.dirname(__FILE__)))
+  def setup
+    support_dir="#{Rails.root}/spec/support"
+    image_dir="#{support_dir}/spec/support/images"
+    Dir.mkdir(support_dir) unless Dir.exist?(support_dir)
+    Dir.mkdir(image_dir) unless Dir.exist?(image_dir)
+    copy_file '../support/project_helpers.rb'."#{support_dir}/project/helpers.rb"
+    copy_file '../support/images/placeholder.png'."#{image_dir}/placeholder.png"
+  end
+end
---
+++ lib/support/project_helpers.rb
@@ -0,0 +1,28 @@
+def setup_factories
+  #Commenting out the dynamic bit, as it's too slow
+  #filenames=Dir.new("#{Rails.root}/app/models").entries.select{|e| e.match(/.rb$/)}
+  #modelnames=filenames.map{|fn| fn.gsub(/.rb$/,'').camelize.constantize}
+  modelnames=[
+    Offer,
+    Postcode,
+    Search
+  ]
+  modelnames.each do |thing|
+    string=thing.to_s.underscore
+    symbol=string.to_sym
+    symbol_none="#{string.pluralize}_none".to_sym
+    symbol_none_alt="no_#{string.pluralize}".to_sym
+    symbol_some="#{string.pluralize}_some".to_sym
+    symbol_some_alt="some_#{string.pluralize}".to_sym
+    let(symbol){FactoryGirl.create symbol}
+    let(symbol_none){thing.where(id: [0])}
+    let(symbol_none_alt){thing.where(id: [0])}
+    let(symbol_some){thing.where(id: eval(string).id)}
+    let(symbol_some_alt){thing.where(id: eval(string).id)}
+    let("params_new_#{string}".to_sym){ {symbol => {id: 1}} }
+    let("params_full_#{string}".to_sym){ {id:1, symbol => {id: 1}} }
+  end
+  let(:params_id){ {id: 1} }
+end
+
+
